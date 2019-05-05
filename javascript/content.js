4d53ab31eac7682f5f01f97fec75b8f764b61faf
Valter Andrei <valterandrey@gmail.com>
2019-05-05 20:21:09 -0300

    update structure

---
+++ javascript/content.js
@@ -0,0 +1,45 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === 'clicked_browser_action' ) {
+
+      let filled_fields = 0;
+
+      $("input[name*='email']").each(function(){
+        $(this).val(chance.email());
+        filled_fields += 1;
+      });
+
+      $("input[name*='name']").each(function(){
+        $(this).val(chance.name());
+        filled_fields += 1;
+      });
+
+      $("input[name*='phone']").each(function(){
+        $(this).val(generatePhoneNumber());
+        filled_fields += 1;
+      });
+
+      $("input[name*='date'], input[name*='birthday']").each(function(){
+        $(this).val(chance.date({string: true, american: false}));
+        filled_fields += 1;
+      });
+
+      $("input[name*='cpf']").each(function(){
+        $(this).val(chance.cpf());
+        filled_fields += 1;
+      });
+
+      $("input[name*='cnpj']").each(function(){
+        $(this).val(generateCNPJ());
+        filled_fields += 1;
+      });
+
+      $("input[name*='cep'], input[name*='zip_code']").each(function(){
+        $(this).val(generateCEP());
+        filled_fields += 1;
+      });
+
+      showToast(filled_fields);
+    }
+  }
+);
---
+++ javascript/show-toast.js
@@ -0,0 +1,27 @@
+function showToast(fields){
+  if (fields == 1){
+    $.toast({
+      heading: 'Sucesso!',
+      text: fields+' campo foi preenchido.',
+      showHideTransition: 'slide',
+      icon: 'success',
+      position: 'bottom-right'
+    })
+  } else if (fields > 1) {
+    $.toast({
+      heading: 'Sucesso!',
+      text: fields+' campos foram preenchidos.',
+      showHideTransition: 'slide',
+      icon: 'success',
+      position: 'bottom-right'
+    })
+  } else {
+    $.toast({
+      heading: 'Ops!',
+      text: 'Nenhum campo foi preenchido.',
+      showHideTransition: 'slide',
+      icon: 'info',
+      position: 'bottom-left'
+    })
+  }
+}
